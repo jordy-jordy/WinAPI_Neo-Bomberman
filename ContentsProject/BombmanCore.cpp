8b5146aba6718e94cd91f20684ec3e3e0fc61c82
Jordy <rinnn1212@gmail.com>
2024-11-05 11:58:35 +0900

    이현정 : 리소스 폴더 정리, 이미지 폴더 경로 수정

--- ContentsProject/BombmanCore.cpp
+++ ContentsProject/BombmanCore.cpp
@@ -27,7 +27,7 @@ void BombmanCore::BeginPlay()
 
 	UEngineDirectory Dir;
 
-	if (false == Dir.MoveParentToDirectory("Resources"))
+	if (false == Dir.MoveParentToDirectory("Resources//Imgs"))
 	{
 		MSGASSERT("리소스 폴더를 찾지 못했습니다.");
 		return;
@@ -44,49 +44,49 @@ void BombmanCore::BeginPlay()
 
 	{
 		UEngineDirectory Title_Basic_Dir;
-		Title_Basic_Dir.MoveParentToDirectory("Resources");
+		Title_Basic_Dir.MoveParentToDirectory("Resources//Imgs");
 		Title_Basic_Dir.Append("01_TITLE//00_00_BASIC");
 		UImageManager::GetInst().LoadFolder(Title_Basic_Dir.GetPathToString());
 	}
 
 	{
 		UEngineDirectory Title_NeoGeo_Dir;
-		Title_NeoGeo_Dir.MoveParentToDirectory("Resources");
+		Title_NeoGeo_Dir.MoveParentToDirectory("Resources//Imgs");
 		Title_NeoGeo_Dir.Append("01_TITLE//00_Neo-Geo_LOGO");
 		UImageManager::GetInst().LoadFolder(Title_NeoGeo_Dir.GetPathToString());
 	}
 	{
 
 		UEngineDirectory Title_Warning_Dir;
-		Title_Warning_Dir.MoveParentToDirectory("Resources");
+		Title_Warning_Dir.MoveParentToDirectory("Resources//Imgs");
 		Title_Warning_Dir.Append("01_TITLE//01_WARNING");
 		UImageManager::GetInst().LoadFolder(Title_Warning_Dir.GetPathToString());
 	}
 	{
 
 		UEngineDirectory Title_Hudson_Dir;
-		Title_Hudson_Dir.MoveParentToDirectory("Resources");
+		Title_Hudson_Dir.MoveParentToDirectory("Resources//Imgs");
 		Title_Hudson_Dir.Append("01_TITLE//02_HUDSON_LOGO");
 		UImageManager::GetInst().LoadFolder(Title_Hudson_Dir.GetPathToString());
 	}
 	{
 
 		UEngineDirectory Title_OP_Animation_Dir;
-		Title_OP_Animation_Dir.MoveParentToDirectory("Resources");
+		Title_OP_Animation_Dir.MoveParentToDirectory("Resources//Imgs");
 		Title_OP_Animation_Dir.Append("01_TITLE//03_OP_ANIMATION");
 		UImageManager::GetInst().LoadFolder(Title_OP_Animation_Dir.GetPathToString());
 	}
 	{
 
 		UEngineDirectory Title_CoinInsert_Dir;
-		Title_CoinInsert_Dir.MoveParentToDirectory("Resources");
+		Title_CoinInsert_Dir.MoveParentToDirectory("Resources//Imgs");
 		Title_CoinInsert_Dir.Append("01_TITLE//04_COIN_INSERT");
 		UImageManager::GetInst().LoadFolder(Title_CoinInsert_Dir.GetPathToString());
 	}
 	{
 
 		UEngineDirectory Title_ChooseStage_Dir;
-		Title_ChooseStage_Dir.MoveParentToDirectory("Resources");
+		Title_ChooseStage_Dir.MoveParentToDirectory("Resources//Imgs");
 		Title_ChooseStage_Dir.Append("01_TITLE//05_CHOOSE_STAGE");
 		UImageManager::GetInst().LoadFolder(Title_ChooseStage_Dir.GetPathToString());
 	}
@@ -95,19 +95,19 @@ void BombmanCore::BeginPlay()
 
 	{
 		UEngineDirectory Play_TILE_Dir;
-		Play_TILE_Dir.MoveParentToDirectory("Resources");
+		Play_TILE_Dir.MoveParentToDirectory("Resources//Imgs");
 		Play_TILE_Dir.Append("02_PLAY//00_Tiles");
 		UImageManager::GetInst().LoadFolder(Play_TILE_Dir.GetPathToString());
 	}
 	{
 		UEngineDirectory Play_BG_Dir;
-		Play_BG_Dir.MoveParentToDirectory("Resources");
+		Play_BG_Dir.MoveParentToDirectory("Resources//Imgs");
 		Play_BG_Dir.Append("02_PLAY//01_Background");
 		UImageManager::GetInst().LoadFolder(Play_BG_Dir.GetPathToString());
 	}
 	{
 		UEngineDirectory Play_Bomb_Dir;
-		Play_Bomb_Dir.MoveParentToDirectory("Resources");
+		Play_Bomb_Dir.MoveParentToDirectory("Resources//Imgs");
 		Play_Bomb_Dir.Append("02_PLAY//02_Objects//01_Bomb");
 		UImageManager::GetInst().LoadFolder(Play_Bomb_Dir.GetPathToString());
 	}
